9bee3fd31c5c2ff64b55690a4765bc7076faeda1
mekni99 <mohameddhia.mekni@esprit.tn>
2024-07-21 21:59:39 +0000

    integration

--- tawasalna-MaintenanceAgent/src/main/java/com/tawasalna/MaintenanceAgent/TawasalnaMaintenanceAgentApplication.java
+++ tawasalna-MaintenanceAgent/src/main/java/com/tawasalna/MaintenanceAgent/TawasalnaMaintenanceAgentApplication.java
@@ -1,12 +1,10 @@
 package com.tawasalna.MaintenanceAgent;
 
-import com.tawasalna.shared.EnableSharedModule;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition
-@EnableSharedModule
 @SpringBootApplication
 public class TawasalnaMaintenanceAgentApplication {
 
