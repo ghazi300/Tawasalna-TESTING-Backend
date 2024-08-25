6b8b7d5e6e5aa2c767753b6763e974db7e9f9579
Ghazi <ghazibouzid63@gmail.com>
2024-08-25 18:44:59 +0100

    pulling final version

--- tawasalna-crisis/src/main/java/com/tawasalna/tawasalnacrisis/TawasalnaCrisisApplication.java
+++ tawasalna-crisis/src/main/java/com/tawasalna/tawasalnacrisis/TawasalnaCrisisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-
+import com.tawasalna.shared.EnableSharedModule;
 @SpringBootApplication
 @EnableMongoAuditing
-@ComponentScan(basePackages = {"com.tawasalna.shared", "com.tawasalna.tawasalnacrisis"})
+@EnableSharedModule
 public class TawasalnaCrisisApplication {
 
     public static void main(String[] args) {
