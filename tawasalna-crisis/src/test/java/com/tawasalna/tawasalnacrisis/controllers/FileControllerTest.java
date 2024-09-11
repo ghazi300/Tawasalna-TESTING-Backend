53afbe9b20a2f14553252a25e078d3690ca863eb
Ghazi <ghazibouzid63@gmail.com>
2024-09-11 19:27:11 +0100

    first commit

--- tawasalna-crisis/src/test/java/com/tawasalna/tawasalnacrisis/controllers/FileControllerTest.java
+++ tawasalna-crisis/src/test/java/com/tawasalna/tawasalnacrisis/controllers/FileControllerTest.java
@@ -21,7 +21,6 @@ import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
 public class FileControllerTest {
 
     @Mock
--- tawasalna-crisis/src/test/java/com/tawasalna/tawasalnacrisis/controllers/NotificationControllerTest.java
+++ tawasalna-crisis/src/test/java/com/tawasalna/tawasalnacrisis/controllers/NotificationControllerTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
 public class NotificationControllerTest {
 
     @Mock
